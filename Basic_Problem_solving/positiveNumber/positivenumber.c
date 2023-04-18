33baa581390e4ee2e2f2bfa3b259b4df619eeadd
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-18 18:17:49 +0600

    positive number

---
+++ Basic_Problem_solving/positiveNumber/positivenumber.c
@@ -0,0 +1,9 @@
+int number = -1;
+if (number > 0)
+{
+    printf("number is positive %d", number);
+}
+else
+{
+    printf("number is negative %d", number);
+}
---
+++ Basic_Problem_solving/positiveNumber/positivenumber.js
@@ -0,0 +1,8 @@
+var number = -2
+
+if (number > 0) {
+    console.log("number is positive", number);
+}
+else {
+    console.log("number is negative ", number);
+}
